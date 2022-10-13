fea4cfa0ede7b6ec8d996e05150131369456329a
GregNewby <arinewby@ymail.com>
2022-10-13 15:30:51 -0500

    first commit

---
+++ Batch63_Git/src/com/cogent/app02/test001.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.cogent.app02;
+
+/**
+ * @author Greg N.
+ * @date :
+ * 
+ */
+public class test001 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
