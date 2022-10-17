c741c60dd268837fa5a9af2d933ab7a3cdd8dc98
GregNewby <arinewby@ymail.com>
2022-10-16 22:24:39 -0500

    sunday

--- Batch63_Git/src/com/cogent/Mini_Project_Implementation/NewmartClient.java
+++ Batch63_Git/src/com/cogent/Mini_Project_Implementation/NewmartClient.java
@@ -4,6 +4,8 @@
 package com.cogent.Mini_Project_Implementation;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import com.cogent.Mini_Project_Implementation.Bean.Product01;
@@ -38,68 +40,101 @@ public class NewmartClient {
 			System.out.println("8. Find expired products");
 			System.out.println("9. Exit\n");
 		
+			String prodname;
+			int prodid;
+			int prodcat;
+			double price;
 		
 			System.out.println("Enter the number of the option you would like to choose: ");
 			int num= scanner.nextInt();
 			switch(num) {
 			case 1:
-				System.out.println("Enter the new product name");
-				scanner.nextLine();
-				String prodname= scanner.nextLine();
-				System.out.println("Enter category code");
-				int prodcat= scanner.nextInt();
-				System.out.println("Enter the manufacture date yyyy-mm-dd");
-				String man= scanner.next();
-				LocalDate manufacture= LocalDate.parse(man);
-				System.out.println("Enter the price of the product");
-				double price= scanner.nextDouble();
-				System.out.println("Enter the expiration date yyyy-mm-dd");
-				String exp= scanner.next();
-				LocalDate expiration= LocalDate.parse(exp);
-				
-				if(srvs.addProduct(new Product01(prodname,prodcat,manufacture,price,expiration))) {
-					System.out.println("Product added!");
-				} else {System.out.println("Product has not been inserted");}
-				
+				try {
+					System.out.println("Enter the new product name");
+					scanner.nextLine();
+					prodname= scanner.nextLine();
+					System.out.println("Enter category code");
+					prodcat= scanner.nextInt();
+					System.out.println("Enter the manufacture date yyyy-mm-dd");
+					String man= scanner.next();
+					LocalDate manufacture= LocalDate.parse(man);
+					System.out.println("Enter the price of the product");
+					price= scanner.nextDouble();
+					System.out.println("Enter the expiration date yyyy-mm-dd");
+					String exp= scanner.next();
+					LocalDate expiration= LocalDate.parse(exp);
+					
+					if(srvs.addProduct(new Product01(prodname,prodcat,manufacture,price,expiration))) {
+						System.out.println("Product added!");
+					} else {System.out.println("Product has not been inserted");}
+				} catch (DateTimeParseException dtpe) {
+					dtpe.printStackTrace();
+				} catch (InputMismatchException ime) {
+					ime.printStackTrace();
+				}
 				break;	
 			case 2:
-				System.out.println("Enter the product ID you would like to delete");
-				int prodid=scanner.nextInt();
-				if(srvs.deleteProductbyID(prodid)) {
-					System.out.println("Product deleted");
-				} else {System.out.println("Product has not been deleted");}
+				try {
+					System.out.println("Enter the product ID you would like to delete");
+					prodid=scanner.nextInt();
+					if(srvs.deleteProductbyID(prodid)) {
+						System.out.println("Product deleted");
+					} else {System.out.println("Product has not been deleted");}
+				} catch (InputMismatchException ime) {
+					ime.printStackTrace();
+				}
 				break;
 			case 3:
-				System.out.println("Enter the catagory code that you would like to delete");
-				prodcat=scanner.nextInt();
-				if(srvs.deleteProductCat(prodcat)) {
-					System.out.println("All product in the category have been deleted");
-				} else {System.out.println("Category has not been deleted");}
+				try {
+					System.out.println("Enter the catagory code that you would like to delete");
+					prodcat=scanner.nextInt();
+					if(srvs.deleteProductCat(prodcat)) {
+						System.out.println("All product in the category have been deleted");
+					} else {System.out.println("Category has not been deleted");}
+				} catch (InputMismatchException ime) {
+					ime.printStackTrace();
+				}
 				break;
 			case 4:
-				System.out.println("Enter the category code to find the cheapest item in the category");
-				prodcat=scanner.nextInt();
-				System.out.println(srvs.findCheapestProductInCat(prodcat));
+				try {
+					System.out.println("Enter the category code to find the cheapest item in the category");
+					prodcat=scanner.nextInt();
+					System.out.println(srvs.findCheapestProductInCat(prodcat));
+				} catch (InputMismatchException ime) {
+					ime.printStackTrace();
+				}
 				break;
 			case 5:
-				System.out.println("Enter the category code that you would like to explain");
-				prodcat=scanner.nextInt();
-				for(Product01 p : srvs.findProductsInCat(prodcat)) {
-					System.out.println(p);
+				try {
+					System.out.println("Enter the category code that you would like to explain");
+					prodcat=scanner.nextInt();
+					for(Product01 p : srvs.findProductsInCat(prodcat)) {
+						System.out.println(p);
+					}
+				} catch (InputMismatchException ime) {
+					ime.printStackTrace();
 				}
 				break;
 			case 6:
-				System.out.println("Enter the product ID of the product that you would like to display");
-				prodid=scanner.nextInt();
-				System.out.println(srvs.findproductByID(prodid));
+				try {
+					System.out.println("Enter the product ID of the product that you would like to display");
+					prodid=scanner.nextInt();
+					System.out.println(srvs.findproductByID(prodid));
+				} catch (InputMismatchException ime) {
+					ime.printStackTrace();
+				}
 				break;
 			case 7:
-				System.out.println("Enter the product ID you would like to update");
-				prodid= scanner.nextInt();
-				System.out.println("Enter the new name for the product");
-				scanner.nextLine();
-				prodname= scanner.nextLine();
-				srvs.updateProductName(prodid, prodname);
+				try {
+					System.out.println("Enter the product ID you would like to update");
+					prodid= scanner.nextInt();
+					System.out.println("Enter the new name for the product");
+					scanner.nextLine();
+					prodname= scanner.nextLine();
+					srvs.updateProductName(prodid, prodname);
+				} catch (InputMismatchException ime) {
+					ime.printStackTrace();
+				}
 				break;
 			case 8:
 				for(Product01 p : srvs.findExpiredProducts(LocalDate.now())) {
