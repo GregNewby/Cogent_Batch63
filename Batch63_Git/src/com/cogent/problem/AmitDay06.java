72ddbb260b06106949ca9f2662991a09525fa705
Greg Newby <arinewby@ymail.com>
2022-12-02 12:14:31 -0600

    12-2-22

--- Batch63_Git/src/com/cogent/problem/AmitDay06.java
+++ Batch63_Git/src/com/cogent/problem/AmitDay06.java
@@ -3,6 +3,17 @@
  */
 package com.cogent.problem;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import org.w3c.dom.Node;
+
 /**
  * @author Greg N.
  * @date :
@@ -10,36 +21,75 @@ package com.cogent.problem;
  */
 public class AmitDay06 {
 	public static void main(String[] args) {
-		int[][] twod = new int[3][3];
+	
+		String s = "HLKsmAsH";
 		
-		twod[0][0] = 3;
-		twod[1][1] = 3;
-		twod[2][2] = 3;
-		twod[0][1] = 3;
-		twod[1][2] = 3;
-
-		AmitDay05.twoDArray(twod);	
-		
-		System.out.println();
-		
-		int[][] threed = new int[4][3];
-		
-		threed[0][0] = 2;
-		threed[1][1] = 2;
-		threed[2][2] = 2;
-		threed[0][1] = 2;
-		threed[1][2] = 2;
-
-		AmitDay05.twoDArray(threed);
+		System.out.println(revCase(s));
 	}
 
 //	1. How do you reverse a string in Java?
+	public static String rev(String s) {
+		String rev = "";
+		
+		for(int i = s.length() -1; i>= 0; i--) {
+			rev = rev + s.charAt(i);
+		}
+		 return rev;
+	}
+	
 //	2. Write a Java program to print a Fibonacci sequence using recursion.
+	public static int fibonacci(int fib) {
+		if(fib <= 1) {
+			return fib;
+		}			
+		return fibonacci(fib-2) + fibonacci(fib-1);		
+	}
+	
 //	3. Write a Java program that sorts HashMap by value.
+	public static HashMap sortByValue(HashMap map) {
+		
+		map.entrySet().stream().sorted(Entry.comparingByValue()).forEach(System.out::println);
+		return map;
+	}
+	
 //	4. How do you get distinct characters and their count in a string in Java?
+	/*
+	 * {	
+	 * 
+	 * String s;
+	 * s.indexOf('y')
+	 *
+	 * }
+	 */
+	
 //	5.  How do you show a diamond problem with multiple inheritance in Java?
 //	6. Write a program in Java to count the digits in a number.
+	public static int digitsInNum(int i) {
+		String length = String.valueOf(i);
+		
+		return length.length();
+	}
+	
 //	7. Write a program in Java to Toggle the case of every character of a string. For instance, if the input string is “CoGEnt”, the output should be “cOgeNT”.
+	public static String revCase(String s) {
+		String toggled = "";
+		for(int i = 0; i < s.length(); i++) {
+			String s2 = String.valueOf(s.charAt(i));
+	
+			if(String.valueOf(s.charAt(i)).equals(s2.toUpperCase())) { // if it is capital
+				toggled = toggled + s2.toLowerCase();
+	
+			} else {
+				toggled = toggled + s2.toUpperCase();
+				
+			}
+			//System.out.println(toggled);
+		}
+		
+		return toggled;
+	}
+	
 //	8.Java program to remove duplicate elements from a singly linked list
+
 
 }
---
+++ Batch63_Git/src/com/cogent/problem/AmitDay07.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.cogent.problem;
+
+/**
+ * @author Greg N.
+ * @date :
+ * 
+ */
+public class AmitDay07 {
+
+//	1) Given an element, how do you find out whether that element exist in a LinkedList or not. If it exist retrieve the position of that element?
+//	2) Write a Java program to traverse the elements of a LinkedList in reverse direction?
+//	3) How do you join an ArrayList at the end of a LinkedList?
+//	4) Write a Java program which implements LinkedList as a Queue (FIFO)?
+//	5) How do you insert an element at the head and tail of a LinkedList?
+//	6) How do you add an element or collection of elements at a specific position of a LinkedList?
+//	7)Write a Java method that will remove a given character from a string object.
+//	8)How can you make a String upper case or lower case in Java?
+//	9)How do you compare two strings in a Java program?
+//	10)How To Remove Duplicate Elements From ArrayList In Java?
+
+}
